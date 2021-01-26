bfb6a3aaf25efe06e0b479dac0f5941053c9d581
VQN <nam.vq@consultindochina.com>
2021-01-26 10:11:01 +0700

    first commit

---
+++ src/app/shared/statistics.ts
@@ -0,0 +1,11 @@
+export interface Frequency {
+    letter: string;
+    frequency: number;
+}
+
+export const STATISTICS: Frequency[] = [
+    { letter: 'A', frequency: .08167 },
+    { letter: 'B', frequency: .01492 },
+    { letter: 'C', frequency: .02782 },
+    { letter: 'D', frequency: .04253 }
+];
---
+++ src/app/shared/stocks.ts
@@ -0,0 +1,16 @@
+export interface Stock {
+    date: number;
+    value: number;
+}
+
+export const STOCKS: Stock[] = [
+    { date: 1, value: 210.73 },
+    { date: 2, value: 214.01 },
+    { date: 3, value: 214.38 },
+    { date: 4, value: 210.97 },
+    { date: 5, value: 210.58 },
+    { date: 6, value: 211.98 },
+    { date: 7, value: 210.11 },
+    { date: 8, value: 207.72 },
+    { date: 9, value: 210.65 }
+];
